76ea861e34859dd02e95413443623e1df53f1ea7
lizim <elizio.neto@outlook.com>
2023-09-06 19:02:29 -0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_chat_gpt/app/chat_app.dart';
 import 'package:my_chat_gpt/home_page/binding/home_page_binding.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/routes/app_routes.dart';
+import 'app/data/firebase/firebase_options.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChatApp(
     titulo: "FastSPDA",
     routes: AppPages.routes,
